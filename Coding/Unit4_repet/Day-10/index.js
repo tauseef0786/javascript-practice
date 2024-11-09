f4a4824599397922d1389278e8ceb236b7fccd24
tauseef0786 <164810977+tauseef0786@users.noreply.github.com>
2024-11-09 22:03:13 +0530

    Delete Coding directory
    it will update

--- Coding/Unit4_repet/Day-10/index.js
+++
@@ -1,7 +0,0 @@
-var a=20;
-function javascript(){
-    console.log(a);
-}
-javascript();
-let a =20;
-console.log(a)
--- Coding/Unit4_repet/Day-8/index.js
+++
@@ -1,7 +0,0 @@
-let name_1;
-name_1="tauseef"
-name_1="raza"
-console.log(name_1)
-let s=""
-console.log(s)
-console.log("5"+2)
