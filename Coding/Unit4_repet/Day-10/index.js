db86cc6fed84ab66b38a94e1395e21df4e69b125
Md Tauseef Raza <tausefm948@gmail.com>
2024-11-09 22:01:06 +0530

    coding

---
+++ Coding/Unit4_repet/Day-10/index.js
@@ -0,0 +1,7 @@
+var a=20;
+function javascript(){
+    console.log(a);
+}
+javascript();
+let a =20;
+console.log(a)
---
+++ Coding/Unit4_repet/Day-8/index.js
@@ -0,0 +1,7 @@
+let name_1;
+name_1="tauseef"
+name_1="raza"
+console.log(name_1)
+let s=""
+console.log(s)
+console.log("5"+2)
